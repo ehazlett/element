892193954f41cee5c40d6dc554f140fec728a58d
Evan Hazlett <ejhazlett@gmail.com>
2020-11-23 15:17:59 -0500

    update protos
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

---
+++ proto.go
@@ -0,0 +1,9 @@
+package element
+
+// go modules are not currently detecting dependencies that are defined in
+// .proto files, this is a workaround to allow them to be defined and vendored
+
+import (
+	_ "github.com/gogo/googleapis/google/api"
+	_ "github.com/gogo/protobuf/gogoproto"
+)
